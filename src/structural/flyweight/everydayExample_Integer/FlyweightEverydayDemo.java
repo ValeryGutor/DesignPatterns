79496ebed49bce9f9ac9feca82f8df12f2e140e8
ValeryGutor <leragutor@i.ua>
2017-02-02 08:02:03 +0200

    Refatoring

--- src/structural/flyweight/everydayExample_Integer/FlyweightEverydayDemo.java
+++ src/structural/flyweight/everydayExample_Integer/FlyweightEverydayDemo.java
@@ -4,8 +4,8 @@ public class FlyweightEverydayDemo {
 
     public static void main(String[] args) {
         // utilize valueOf method :
-        Integer firstInt = Integer.valueOf(5); // valueOf uses flyweight pattern
-        Integer secondInt = Integer.valueOf(5);
+        Integer firstInt = Integer.valueOf(127); // valueOf uses flyweight pattern
+        Integer secondInt = Integer.valueOf(127); // > 128 - no
         Integer thirdInt = Integer.valueOf(10);
         // hashcode of first object and second object is the same;
         System.out.println(System.identityHashCode(firstInt));
